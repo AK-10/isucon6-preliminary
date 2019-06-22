8a6a9f993c720856f0cf24d0cb0c8b8f3a692b2d
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-06-22 12:30:42 +0900

    roleback

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -330,21 +330,21 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 		keywords = append(keywords, regexp.QuoteMeta(entry.Keyword))
 	}
 	// (k1|k2|k3|k4...|kn)
-	// re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
-	pairList := make([]string, 0, 1000)
-	kw2sha := make(map[string]string)
-	for i, kw := range keywords {
-		value := "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		kw2sha[kw] = value
-		pairList[i*2], pairList[i*2+1] = kw, value
-	}
-	rs := strings.NewReplacer(pairList...)
-	content = rs.Replace(content)
+	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+	// pairList := make([]string, 0, 1000)
 	// kw2sha := make(map[string]string)
-	// content = re.ReplaceAllStringFunc(content, func(kw string) string {
-	// 	kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-	// 	return kw2sha[kw]
-	// })
+	// for i, kw := range keywords {
+	// 	value := "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+	// 	kw2sha[kw] = value
+	// 	pairList[i*2], pairList[i*2+1] = kw, value
+	// }
+	// rs := strings.NewReplacer(pairList...)
+	// content = rs.Replace(content)
+	kw2sha := make(map[string]string)
+	content = re.ReplaceAllStringFunc(content, func(kw string) string {
+		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+		return kw2sha[kw]
+	})
 	content = html.EscapeString(content)
 	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
