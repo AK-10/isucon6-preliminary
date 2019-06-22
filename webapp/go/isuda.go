12dbed93d29feb2511d37b8653d5d68d3097277b
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-06-22 12:03:22 +0900

    replacer

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -329,12 +329,18 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 	for _, entry := range entries {
 		keywords = append(keywords, regexp.QuoteMeta(entry.Keyword))
 	}
+	// (k1|k2|k3|k4...|kn)
 	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
-	kw2sha := make(map[string]string)
-	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
+	pairList := make([]string, 0, 1000)
+	for i, kw := range keywords {
+		pairList[i*2], pairList[i*2+1] = kw, "isuda_"+fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+	}
+	content = strings.NewReplacer(pairList...)
+	// kw2sha := make(map[string]string)
+	// content = re.ReplaceAllStringFunc(content, func(kw string) string {
+	// 	kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+	// 	return kw2sha[kw]
+	// })
 	content = html.EscapeString(content)
 	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
