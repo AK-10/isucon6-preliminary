c3141ce71a2f96f49a1bf7c2b6912674e6745ed6
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-07-04 17:12:52 +0900

    revert

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -405,15 +405,13 @@ func keywordByKeywordDeleteHandler(w http.ResponseWriter, r *http.Request) {
 
 func initReplacer() {
 	keywords := getKeywordsByDesc()
-	newKeywordPairList := make([]string, 0)
+	newKeywordPairList = make([]string, 0)
 	for _, kw := range keywords {
 		kw = regexp.QuoteMeta(kw)
 		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		newKeywordPairList = append(newKeywordPairList, kw)
-		newKeywordPairList = append(newKeywordPairList, kw2sha[kw])
+		keywordPairList = append(keywordPairList, kw)
+		keywordPairList = append(keywordPairList, kw2sha[kw])
 	}
-
-	keywordPairList = newKeywordPairList
 }
 
 func removePair(strs []string, search string) []string {
