7186d5ff059f8cdc52030ee6f11f9cadd154a8b3
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-07-04 17:23:15 +0900

    fix initReplacer

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -410,6 +410,7 @@ func initReplacer() {
 		keywordPairList = append(keywordPairList, kw)
 		keywordPairList = append(keywordPairList, kw2sha[kw])
 	}
+	rs = strings.NewReplacer(keywordPairList...)
 }
 
 func removePair(strs []string, search string) []string {
@@ -424,6 +425,10 @@ func removePair(strs []string, search string) []string {
 		}
 	}
 	return result
+}
+
+func updateReplacerWithPost(keyword string) {
+
 }
 
 func updateReplacerWithDelete(keyword string) {
